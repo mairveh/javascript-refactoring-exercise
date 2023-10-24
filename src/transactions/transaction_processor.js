6af63d4110d0e6a982d6f00b7ef79f1a10228374
Merve <babutine@yahoo.com>
2023-10-24 20:20:38 +0100

    refactor: implement some refactoring basics

--- src/transactions/transaction_processor.js
+++ src/transactions/transaction_processor.js
@@ -1,52 +1,66 @@
-var txr = [];
+//remove var and declare inside processTransactions
+//var txr = [];
 
-function processTransactions(transActions) {
+//replace transActions with transactions
+function processTransactions(transactions) {
 
-    txr = [];
+    //add const for  and rename as txArray
+    const txArray = [];
 
-    if(!validateTransactions(transActions)) {
+    //replace below function with a simple if logic
+    /*if(!validateTransactions(transactions)) {
         throw new Error("Undefined collection of transactions")
-    }
+    }*/
+    if (transactions===undefined)
+        throw new Error("Undefined collection of transactions")
 
     let txCount = {}
 
-    const numberOfTransactions = transActions.length;
+    //adding below in the for loop header
+   //const numberOfTransactions = transactions.length;
 
-    for(var i = 0; i < numberOfTransactions; i++) {
-        const transaction = transActions[i];
+    //change var to let
+    //add transactions.length in the for loop header
+    for(let i = 0; i < transactions.length; i++) {
+        const transaction = transactions[i];
         txCount[transaction] ? txCount[transaction] += 1 : txCount[transaction] = 1;
     }
 
     txCount = sortByAmountThenName(txCount);
     
     // Place them back in array for returning
+    //replace for eaxch with map
+    //Object.keys(txCount).map((key) => `${key} ${txCount[key]}`)
+    Array.from(txCount)
     Object.keys(txCount).forEach(function (key, index) {
-        txr[index] = `${key} ${txCount[key]}`;
+        txArray[index] = `${key} ${txCount[key]}`;
     });
 
-    return txr;
+    return txArray;
 }
 
 function sortByAmountThenName(txCount) {
-    let sortedKeys = Object.keys(txCount).sort(function sortingFunction(itemOne, itemTwo) {
+    //replace let with const
+    const sortedKeys = Object.keys(txCount).sort(function sortingFunction(itemOne, itemTwo) {
         return  txCount[itemTwo] - txCount[itemOne] || itemOne > itemTwo || -(itemOne < itemTwo)}
     );
 
-    let sortedResults = {};
-    for(let objectKey of sortedKeys) {
+    //replace 2 lets with const
+    const sortedResults = {};
+    for(const objectKey of sortedKeys) {
         sortedResults[objectKey] = txCount[objectKey];
     }
 
     return sortedResults;
 }
 
-
-function validateTransactions(transactions) {
+//blocking bc this functionality is added into processTransactions function 
+/*function validateTransactions(transactions) {
     if(transactions === undefined) {
         return false;
     } 
 
     return true;
-}
+}*/
 
 module.exports = processTransactions;
