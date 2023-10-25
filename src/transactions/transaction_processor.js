cb289b5843f9fda061ae835770b5434184ba0181
Merve <babutine@yahoo.com>
2023-10-25 20:39:06 +0100

    refactor: update sort function with array sort and map

--- src/transactions/transaction_processor.js
+++ src/transactions/transaction_processor.js
@@ -8,25 +8,11 @@ function processTransactions(transactions) {
         return acc;
     }, {});
 
-    txCount = sortByAmountThenName(txCount);
-    
     let txArray = [];
-    txArray = Object.keys(txCount).map((key) => `${key} ${txCount[key]}`)
+    txArray = Object.keys(txCount).sort((itemOne, itemTwo) => (txCount[itemTwo] - txCount[itemOne] || itemOne > itemTwo || -(itemOne < itemTwo)))
+    txArray = txArray.map((key) => `${key} ${txCount[key]}`)
 
     return txArray;
-}
-
-function sortByAmountThenName(txCount) {
-    const sortedKeys = Object.keys(txCount).sort(function sortingFunction(itemOne, itemTwo) {
-        return  txCount[itemTwo] - txCount[itemOne] || itemOne > itemTwo || -(itemOne < itemTwo)}
-    );
-
-    const sortedResults = {};
-    for(const objectKey of sortedKeys) {
-        sortedResults[objectKey] = txCount[objectKey];
-    }
-
-    return sortedResults;
 }
 
 module.exports = processTransactions;
