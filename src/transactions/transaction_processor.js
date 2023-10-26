b87631f2def2bc1a13498bec60e61c7689adfbc8
Merve <babutine@yahoo.com>
2023-10-26 12:35:35 +0100

    refactor: rename variable and change to const

--- src/transactions/transaction_processor.js
+++ src/transactions/transaction_processor.js
@@ -3,13 +3,13 @@ function processTransactions(transactions) {
     if (transactions===undefined)
         throw new Error("Undefined collection of transactions")
 
-    let txCount = transactions.reduce((acc, curr) => {
+    const txCountMap = transactions.reduce((acc, curr) => {
         acc[curr]?(acc[curr]++):(acc[curr]=1);
         return acc;
     }, {});
 
-    return Object.keys(txCount).sort((itemOne, itemTwo) => (txCount[itemTwo] - txCount[itemOne] || itemOne > itemTwo || -(itemOne < itemTwo)))
-    .map((key) => `${key} ${txCount[key]}`)
+    return Object.keys(txCountMap).sort((itemOne, itemTwo) => (txCountMap[itemTwo] - txCountMap[itemOne] || itemOne > itemTwo || -(itemOne < itemTwo)))
+    .map((key) => `${key} ${txCountMap[key]}`)
 }
 
 module.exports = processTransactions;
